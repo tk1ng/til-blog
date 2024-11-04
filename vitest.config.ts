4f734d639e7a8cf5a52eaac9426542ff8d7b3936
Taryn King <marshaeking@gmail.com>
2024-11-03 23:11:11 -0600

    config: correct file path in config file

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     test: {
-        include: ['**/*.test.{js,ts,jsx,tsx}', '**/*.spec.{js,ts,jsx,tsx}'],
+        include: ['src/**/*.test.{js,ts,jsx,tsx}', '**/*.spec.{js,ts,jsx,tsx}'],
         exclude: ['node_modules', 'dist'],
         environment: 'jsdom',
     }
