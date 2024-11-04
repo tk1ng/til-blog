958800a5968f1ffb5fc123778d61355f63953578
Taryn King <marshaeking@gmail.com>
2024-11-03 23:04:46 -0600

    config: add target files for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     test: {
+        include: ['**/*.test.{js,ts,jsx,tsx}', '**/*.spec.{js,ts,jsx,tsx}'],
+        exclude: ['node_modules', 'dist'],
         environment: 'jsdom',
     }
 });
