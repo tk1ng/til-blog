75fa797332a506069f5a4942aafacac643dca056
Taryn King <marshaeking@gmail.com>
2024-11-04 18:47:52 -0600

    config: correct coverage workflow and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
             extension: [".tsx"],
             reporter: ['text', 'json-summary', 'json'],
             reportsDirectory: './coverage',
+            reportOnFailure: true,
         },
     },
 });
