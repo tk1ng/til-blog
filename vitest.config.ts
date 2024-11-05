c60fb25ef07caef9061f45721cba8f7e58d562fa
Taryn King <marshaeking@gmail.com>
2024-11-04 18:37:55 -0600

    config: configure coverage report in workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,5 +7,11 @@ export default defineConfig({
         include: ['src/**/*.test.{js,ts,jsx,tsx}', '**/*.spec.{js,ts,jsx,tsx}'],
         exclude: ['node_modules', 'dist'],
         environment: 'jsdom',
-    }
+        coverage: {
+            provider: 'v8',
+            extension: [".tsx"],
+            reporter: ['text', 'json-summary', 'json'],
+            reportsDirectory: './coverage',
+        },
+    },
 });
